8555660c7da957be30497ae826e3ae5d88dc4b4d
franklin garcia <franklin__garcia@unitec.edu>
2017-02-24 14:35:36 -0600

    Haciendo entorno

--- lab5_franklin_garcia_progra2/src/lab5_franklin_garcia_progra2/canchas.java
+++ lab5_franklin_garcia_progra2/src/lab5_franklin_garcia_progra2/canchas.java
@@ -9,6 +9,39 @@ package lab5_franklin_garcia_progra2;
  *
  * @author Franklin Garcia
  */
-public class canchas {
+public class canchas extends lugar{
+   protected String categoria;
+   protected String estado;
+
+    public canchas() {
+        super();
+    }
+
+    public canchas(String categoria, String estado, String nombre, String direccion, String entradas, String salida, int seguridad) {
+        super(nombre, direccion, entradas, salida, seguridad);
+        this.categoria = categoria;
+        this.estado = estado;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
     
 }
--- lab5_franklin_garcia_progra2/src/lab5_franklin_garcia_progra2/restaurantes.java
+++ lab5_franklin_garcia_progra2/src/lab5_franklin_garcia_progra2/restaurantes.java
@@ -9,6 +9,38 @@ package lab5_franklin_garcia_progra2;
  *
  * @author Franklin Garcia
  */
-public class restaurantes {
+public class restaurantes  extends lugar{
+protected  String categoria;
+protected int calificacion;
+    public restaurantes() {
+        super();
+    }
+
+    public restaurantes(String categoria, int calificacion, String nombre, String direccion, String entradas, String salida, int seguridad) {
+        super(nombre, direccion, entradas, salida, seguridad);
+        this.categoria = categoria;
+        this.calificacion = calificacion;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(int calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
     
 }
