35e5e07ed1fbd75981ec88151d3c2ab57f6a7014
franklin garcia <franklin__garcia@unitec.edu>
2017-02-24 13:49:03 -0600

    Haciendo clases

--- lab5_franklin_garcia_progra2/src/lab5_franklin_garcia_progra2/canchas.java
+++ lab5_franklin_garcia_progra2/src/lab5_franklin_garcia_progra2/canchas.java
@@ -9,13 +9,6 @@ package lab5_franklin_garcia_progra2;
  *
  * @author Franklin Garcia
  */
-public class Lab5_franklin_garcia_progra2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class canchas {
     
 }
